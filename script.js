1de5a4739180f57d912e28cc9d42e47dcfc8269a
HamzaJirah <hamzajirah@gmail.com>
2018-09-12 08:27:48 +0100

    comment previous codes

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //VARIABLES
 
 //'var' keyword
-var age = 23;
-age = 28;
+// var age = 23;
+// age = 28;
 //console.log(age); //28
 
 //'const' keyword
-const age1 = 30;
+// const age1 = 30;
 //age1 = 32;
 //console.log(age1); //Uncaught TypeError: Assignment to constant variable.
 //Note: with const, you can't reassigned the value of a variable
 
 //using const in obect naming
-const person = {
-  name: 'abc',
-}
-person.age = 23;
+// const person = {
+//   name: 'abc',
+// }
+// person.age = 23;
 // console.log(person);
 //Note: with objects and arrays, you can declare an object/array an add properties/elements to it.
 
