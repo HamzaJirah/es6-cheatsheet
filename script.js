e7246d79c2a4ca84cc62a5d6cd09761530a27a37
HamzaJirah <hamzajirah@gmail.com>
2018-09-12 07:45:14 +0100

    added console message

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 //'var' keyword
 var age = 23;
 age = 28;
-console.log(age);
+console.log(age); //28
 
 //'const' keyword
 const age1 = 30;
 age1 = 32;
-console.log(age1);
+console.log(age1); //Uncaught TypeError: Assignment to constant variable.
+
