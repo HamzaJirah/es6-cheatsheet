b27dd9b124dae713f5e63e413f8d2c471c682473
HamzaJirah <hamzajirah@gmail.com>
2018-09-13 05:27:53 +0100

    created thisOne arrow function

--- script.js
+++ script.js
@@ -90,4 +90,5 @@ console.log(studentOne);*/ //Uncaught SyntaxError: Missing initializer in const 
 // console.log(multNums(4, 9));
 
 //Arrow function and this
+const thisOne = () => console.log(this);
 
